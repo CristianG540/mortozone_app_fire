16376dea77218ad3bd3438e40236b702cdfd03bc
CristianG540 <cristianneatproy@gmail.com>
2018-06-18 12:21:59 -0500

    first commit

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,146 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs/Observable';
+import { Events } from 'ionic-angular';
+import 'rxjs/add/operator/switchMap';
+import { map, timeout } from 'rxjs/operators';
+
+// libs terceros
+import Raven from 'raven-js';
+
+// AngularFire - Firebase
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Models
+import { User } from './model/user';
+
+// Providers
+import { ConfigProvider as cg } from '../config/config';
+
+@Injectable()
+export class AuthProvider {
+
+  public userSession;
+  private _userRef: AngularFireObject<User>;
+  public userObservable: Observable<User>;
+  public userData: User;
+  public emailStatus: boolean = false; // Me indica si el usuario ya verifico la cuenta
+
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private angularFireDB: AngularFireDatabase,
+    private util: cg,
+    private evts: Events,
+    private storage: Storage,
+    private httpClient: HttpClient,
+  ) {
+    this.userObservable = this.angularFireAuth.authState.switchMap(
+      user => {
+        if (user) {
+          this.userSession = user;
+          this.emailStatus = user.emailVerified;
+          this._userRef = this.angularFireDB.object(`users/${user.uid}`);
+          /**
+           * Hago el map aqui para evitar poner codigo fuera del provider
+           * al momento de hacer el subscribe
+           */
+          return this._userRef.valueChanges().pipe(
+            map((userDB: User) => {
+              if (userDB && this.userSession) {
+                this.userData = userDB;
+                this.sendEmailVerification(this.userSession);
+              }
+              return userDB;
+            }),
+          );
+        } else {
+          return Observable.of(null);
+        }
+      },
+    );
+
+  }
+
+  private setUserData(data: User) {
+    this._userRef.set(data);
+  }
+
+  private updateUserData(data: any) {
+    this._userRef.update(data);
+  }
+
+  public register( d ): Promise<any> {
+    return this.angularFireAuth.auth.createUserWithEmailAndPassword(d.email, d.password).then(user => {
+
+      this._userRef = this.angularFireDB.object(`users/${user.uid}`);
+      this.setUserData({
+        uid: user.uid,
+        name: d.name,
+        username: d.username,
+        email: user.email,
+        idAsesor: d.asesor_id,
+        nitCliente: d.nit_cliente,
+        verificationEmailIsSend: false,
+      });
+
+    });
+  }
+
+  public sendEmailVerification(user): void {
+
+    if (!this.userData.verificationEmailIsSend) {
+
+      user.sendEmailVerification()
+      .then(() => {
+        console.log('email verification sent');
+        this.updateUserData({
+          verificationEmailIsSend: true,
+        });
+      }).catch(err => {
+        console.error('Error sendEmailVerification- providers/auth.ts', err);
+        Raven.captureException( new Error(`Error sendEmailVerification- providers/auth.ts 🐛: ${JSON.stringify(err)}`), {
+          extra: err,
+        });
+      });
+
+    }
+
+  }
+
+  public login(email, password): Promise<any> {
+    return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  public logout(): Promise<any> {
+    return this.removeTokenJosefa().then(res => {
+      this.evts.publish('auth:logout', '');
+      return this.angularFireAuth.auth.signOut();
+    });
+  }
+
+  public async getTokenJosefa(): Promise<any> {
+    const auth: string = 'Basic ' + btoa('admin:admin1234');
+    const options = {
+      headers: new HttpHeaders({
+        'Accept'       : 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization': auth,
+      }),
+    };
+    const url: string = cg.JOSEFA_URL + '/authenticate';
+
+    const res = await this.httpClient.post<any>( url, '', options ).pipe(
+      timeout(7000),
+    ).toPromise();
+
+    return await this.storage.set('josefa-token', res.data.token);
+
+  }
+
+  public removeTokenJosefa(): Promise<any> {
+    return this.storage.remove('josefa-token');
+  }
+
+}
---
+++ src/providers/productos/productos.ts
@@ -0,0 +1,131 @@
+import { Injectable } from '@angular/core';
+import { Events } from 'ionic-angular';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
+
+// Libs terceros
+import * as _ from 'lodash';
+import Raven from 'raven-js';
+
+// AngularFire - Firebase
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import * as firebase from 'firebase';
+
+// Models
+import { Producto } from './models/producto';
+import { CarItem } from '../carrito/models/carItem';
+
+// Providers
+import { ConfigProvider as cg } from '../config/config';
+
+@Injectable()
+export class ProductosProvider {
+
+  public productos: Producto[];
+  public sku$: BehaviorSubject<string|null>;
+
+  constructor(
+    private angularFireDB: AngularFireDatabase,
+    private evts: Events,
+  ) {
+  }
+
+  public init (): void {
+    this.sku$ = new BehaviorSubject(null);
+    const prodsObserv: Observable<any> = this.sku$.switchMap(sku => {
+
+      return this.angularFireDB.list(`products/`, ref => {
+        return sku ? ref.orderByKey().startAt(sku).endAt(sku + '\uffff').limitToFirst(100) : ref.limitToFirst(100);
+      }).valueChanges();
+
+    });
+
+    const prodsSub: Subscription = prodsObserv.subscribe(
+      prods => {
+        this.productos = prods;
+      },
+      err => {
+        console.error('error init - providers/productos.ts', err);
+        Raven.captureException( new Error(`error init - providers/productos.ts 🐛: ${JSON.stringify(err)}`), {
+          extra: err,
+        });
+      },
+    );
+    this.evts.subscribe('auth:logout', () => {
+      prodsSub.unsubscribe();
+    });
+  }
+
+  public async fetchProdsByids( ids: string[] ): Promise<Producto[]> {
+
+    const prodPromises: Promise<any>[] = _.map(ids, (v, k, l) => {
+      return firebase.database().ref(`products/${v}`).once('value');
+    });
+
+    const prodsSnapshots = await Promise.all(prodPromises);
+
+    return _.map(prodsSnapshots, (snapshot: any) => {
+
+      const producto: Producto = snapshot.val();
+
+      /**
+       * esta validacion la hago por si se elimina un producto de la bd
+       * por falta de existencias, a veces pasaba que si habia un producto
+       * en el carrito y casualmente se elimina, ocurria un error donde
+       * no se encontraba el _id
+       */
+      if ( _.has(producto, '_id')) {
+        return producto;
+      } else {
+        return new Producto(
+          snapshot.key,
+          'producto agotado',
+          'producto agotado',
+          'https://www.igbcolombia.com/app/www/assets/img/logo/logo_igb_small.png',
+          null,
+          '',
+          'UND',
+          0,
+          0,
+          '',
+        );
+      }
+
+    });
+  }
+  /**
+   * Esta funcion se encarga de actualizar los productos en firebase al crear una orden
+   *
+   * @param {CarItem[]} carItems
+   * @returns {Promise<any>}
+   * @memberof ProductosProvider
+   */
+  public async updateQuantity(carItems: CarItem[] ): Promise<any> {
+    // Declaro la referencia de los productos para actualizarlos mas adelante
+    const prodsRef: AngularFireList<any> = this.angularFireDB.list(`products/`);
+    // en un array guardo solo los ids de los productos del carrito
+    const prodsId = _.map(carItems, '_id');
+
+    // Busco los productos del carrito en firebase por sus ids
+    const productos: Producto[] = await this.fetchProdsByids(prodsId);
+
+    // en un array guardo una a una las promsesas de actualizacion de cada producto
+    const updatePromises: Promise<any>[] = _.map(productos, (prod: Producto) => {
+      const itemId = cg.binarySearch(carItems, '_id', prod._id);
+
+      return prodsRef.update(prod._id, {
+        existencias: prod.existencias - carItems[itemId].cantidad,
+        origen: 'app',
+        updated_at: Date.now(),
+      });
+    });
+
+    // ejecuto todas las promesas del array y devuelvo los valores que devuelven dichas promesas
+    return await Promise.all(updatePromises);
+
+  }
+
+}
